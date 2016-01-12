996b060a7044de3af88606ebc6d5081dfb970ed0
folkien <folkus@gmail.com>
2016-01-12 23:31:32 +0100

    Still testing...

--- main.cpp
+++ main.cpp
@@ -1,56 +1,101 @@
-#include <iostream>
-#include <fstream>
 #include <stdio.h>
-
-using namespace std;
-
-#define byte unsigned char
-#define word unsigned short
-#define dword unsigned int
-#define qword unsigned long
+#include <unistd.h>
+#include <fcntl.h>
 
 
-struct bitmapHeader {
-    unsigned char bfType[2];
-    dword bfSize;
-    word bfReserved1;
-    word bfReserved2;
-    dword bfOffBits;
-    //40 byte header
-    dword biSize;
-    dword biWidth;
-    dword biHeight;
-    word biPlanes;
-    word biBitCount;
-    dword biCompression;
-    dword biSizeImage;
-    dword biXPelsPerMeter;
-    dword biYPelsPerMeter;
-    dword biClrUsed;
-    dword biClrImportant;
+#define byte unsigned char /* 1 */
+
+union _word {
+ char array[2];
+ unsigned short all;
 };
 
-void print_bitmapHeader(struct bitmapHeader *p) {
-    printf("BM = %c%c.\n", p->bfType[0], p->bfType[1]);
-    printf("File length : %db.\n",p->bfSize);
-    printf("RSVD1 %x, RSVD2 %x.\n",p->bfReserved1, p->bfReserved2);
-    printf("Offset ");
+union _dword {
+ char array[4];
+ unsigned int all;
+};
+
+
+struct TbitmapHeader {
+    _word bfType;
+    _dword bfSize;
+    _word bfReserved1;
+    _word bfReserved2;
+    _dword bfOffBits;
+    //40 byte header
+    _dword biSize;
+    _dword biWidth;
+    _dword biHeight;
+    _word biPlanes;
+    _word biBitCount;
+    _dword biCompression;
+    _dword biSizeImage;
+    _dword biXPelsPerMeter;
+    _dword biYPelsPerMeter;
+    _dword biClrUsed;
+    _dword biClrImportant;
+};
+
+void print_hex_array(char *array, unsigned int size) {
+     unsigned int i;
+     printf("%lu: ", array);
+     for (i=0; i<size; i++) {
+         printf("%2.2x",array[i]);
+     }
+     printf(" (%db)\n",size);
 }
+
+void print_bitmapHeader(struct TbitmapHeader *p) {
+    printf("bfType ", p->bfType);
+    print_hex_array(p->bfType.array, sizeof(p->bfType.array));
+    printf("bfSize ",p->bfSize);
+    print_hex_array(p->bfSize.array, sizeof(p->bfSize.array));
+    printf("RSVD1 %4.4x\nRSVD2 %4.4x - ",p->bfReserved1, p->bfReserved2);
+    print_hex_array(p->bfReserved2.array, sizeof(p->bfReserved2.array));
+    printf("Offset %d[%2.2x] - ",p->bfOffBits,p->bfOffBits);
+    print_hex_array(p->bfOffBits.array, sizeof(p->bfOffBits.array));
+    // ---
+    printf("biSize %4.4x - ", p->biSize);
+    print_hex_array(p->biSize.array, sizeof(p->biSize.array));
+    printf("biWidth %4.4x - ", p->biWidth);
+    print_hex_array(p->biWidth.array, sizeof(p->biWidth.array));
+}
+
+struct Ttest {
+     _word a,b,c;
+};
 
 
 int main(int argc, char *argv[]) {
 
-    ifstream fileInput(argv[1]);
-    struct bitmapHeader _bmpHeader;
-    fileInput.read((char *)&_bmpHeader, sizeof(struct bitmapHeader));
-    fileInput.close();
+    int fileInput;
+    struct TbitmapHeader  *bitmapHeader;
+    struct Ttest test;
+    unsigned char array[sizeof(TbitmapHeader)];
+    int i;
 
-    print_bitmapHeader(&_bmpHeader);
 
-    cout << " byte " << sizeof(byte) << endl;
-    cout << " word " << sizeof(word) << endl;
-    cout << " dword " << sizeof(dword) << endl;
-    cout << " qword " << sizeof(qword) << endl;
-    cout << " bitmap Header " << sizeof(struct bitmapHeader) << endl;
+    printf("a ");
+    print_hex_array(test.a.array, sizeof(test.a.array));
+    printf("b ");
+    print_hex_array(test.b.array, sizeof(test.b.array));
+    printf("c ");
+    print_hex_array(test.c.array, sizeof(test.c.array));
+
+    fileInput = open(argv[1], O_RDONLY);
+    read(fileInput, array, sizeof(TbitmapHeader));
+    printf("Readed %d.\n", sizeof(TbitmapHeader));
+    for (i=0; i<sizeof(TbitmapHeader); i++ ) {
+        printf("%2.2x", array[i]);
+        if (i%2 == 1) printf(" ");
+    }
+    printf("\n");
+    close(fileInput);
+
+    bitmapHeader = (struct TbitmapHeader *)&array;
+
+
+    print_bitmapHeader(bitmapHeader);
+
     return 0;
 }
